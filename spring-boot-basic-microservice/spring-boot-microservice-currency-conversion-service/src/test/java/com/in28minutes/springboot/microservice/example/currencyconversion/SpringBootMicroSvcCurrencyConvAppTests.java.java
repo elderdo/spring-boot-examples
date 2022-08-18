015b488a48f5b8aa233e78a366126cdccf5df9a2
dse <elderdo@yahoo.com>
2022-08-18 14:59:01 -0500

    shortened name for Windows

--- spring-boot-basic-microservice/spring-boot-microservice-currency-conversion-service/src/test/java/com/in28minutes/springboot/microservice/example/currencyconversion/SpringBootMicroSvcCurrencyConvAppTests.java.java
+++ spring-boot-basic-microservice/spring-boot-microservice-currency-conversion-service/src/test/java/com/in28minutes/springboot/microservice/example/currencyconversion/SpringBootMicroSvcCurrencyConvAppTests.java.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootMicroserviceCurrencyConversionApplicationTests {
+public class SpringBootMicroSvcCurrencyConvAppTests {
 
 	@Test
 	public void contextLoads() {
