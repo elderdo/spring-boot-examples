5bb2447c6e1db994a18fdb5cd7ac04399f324bc8
dse <elderdo@yahoo.com>
2022-08-18 15:09:05 -0500

    shortened name for windows

--- spring-boot-basic-microservice/spring-boot-microservice-eureka-naming-server/src/test/java/com/in28minutes/springboot/microservice/eureka/naming/server/SpringBootMicroSvcEMSvrAppTests.java
+++ spring-boot-basic-microservice/spring-boot-microservice-eureka-naming-server/src/test/java/com/in28minutes/springboot/microservice/eureka/naming/server/SpringBootMicroSvcEMSvrAppTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootMicroserviceEurekaNamingServerApplicationTests {
+public class SpringBootMicroSvcEMSvrAppTests {
 
 	@Test
 	public void contextLoads() {
